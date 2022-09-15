e2cd71eeb6833a9cab57bb4d1bbd8f98b645f183
Anurag Bhardwaj <anuragbhardwaj@Anurags-MacBook-Pro.local>
2022-09-15 12:59:22 +0530

    ci integration: add hello world api

--- src/main/java/com/example/cart_service/CartServiceApplication.java
+++ src/main/java/com/example/cart_service/CartServiceApplication.java
@@ -6,12 +6,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @SpringBootApplication
 @RestController
 public class CartServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CartServiceApplication.class, args);
+	}
+
+	@PostMapping("/cart")
+	public void createCart(@RequestBody CreateCartRequestBody createCartRequestBody,  HttpServletResponse response) {
+		response.setStatus(HttpServletResponse.SC_ACCEPTED);
 	}
 
 }
